521f078fc33e63a491764f74ec9364689e2fd1d8
isabella <isabella.2010.12@hotmail.com>
2019-10-29 17:32:13 -0500

    primera intervencion

---
+++ archivo2.py
@@ -0,0 +1,7 @@
+def principal():
+    nom=input("ingrese su nombre")
+    ap=input("ingrese su apellido")
+    cel= str(input("ingrese su celular"))
+    print("su nombre es:\n",nom,ap,)
+    print("su celular:\n",cel)
+principal()
